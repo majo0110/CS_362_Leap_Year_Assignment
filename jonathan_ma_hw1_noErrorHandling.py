69e25e0a5077ecedc3c7c8edb5bca196b611ce4f
Jonathan Ma <majo@oregonstate.edu>
2021-04-25 17:38:39 -0700

    Leap year program that has no error handling

---
+++ jonathan_ma_hw1_noErrorHandling.py
@@ -0,0 +1,22 @@
+def main():
+    userInput = input("Enter a year: ")
+    isLeapMsg = "is a leap year."
+    notLeapMsg = "is not a leap year."
+
+    userInt = int(userInput)
+    if userInt % 4 == 0:
+        if userInt % 400 == 0:
+            print(userInt, isLeapMsg)
+        else:
+            if userInt % 100 == 0:
+                print(userInt, notLeapMsg)
+            else:
+                print(userInt, isLeapMsg)
+
+    else:
+        print(userInt, notLeapMsg)
+
+
+
+if __name__ == "__main__":
+    main()
